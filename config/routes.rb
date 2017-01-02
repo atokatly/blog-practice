d1ce22cf0da0b32ea5a95811cd1a04274a5b03f5
Anthony Tokatly <atokatly884@gmail.com>
2017-01-02 14:40:07 -0800

    change default route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "posts#index"
-
+  # root to: "posts#index"
+  get '/' => 'posts#new'
   resources :posts
 end
