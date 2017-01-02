51f58f61990e6f86bdeea8374f9b65f262f9b639
Anthony Tokatly <atokatly884@gmail.com>
2017-01-02 14:34:59 -0800

    rollback db

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  mount Ckeditor::Engine => '/ckeditor'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "posts#new"
+  root to: "posts#index"
 
   resources :posts
 end
